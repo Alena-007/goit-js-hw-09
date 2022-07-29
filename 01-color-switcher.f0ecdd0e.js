46eb788f8688854ab0db1a91e443879ef474fd48
Alena-007 <66263553+Alena-007@users.noreply.github.com>
2022-07-29 15:48:32 +0000

    Deploying to gh-pages from @ Alena-007/goit-js-hw-09@889f546ac9c37db35477ff3ce72d4cd5de81f7b1 🚀

---
+++ 01-color-switcher.f0ecdd0e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){o=setInterval((function(){t&&t.setAttribute("disabled","disabled"),n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.f0ecdd0e.js.map
