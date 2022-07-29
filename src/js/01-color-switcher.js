889f546ac9c37db35477ff3ce72d4cd5de81f7b1
Alena-007 <alena.tv@ukr.net>
2022-07-29 18:47:54 +0300

    add solution task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+btnStartEl.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  timerId = setInterval(() => {
+    if (btnStartEl) {
+      btnStartEl.setAttribute('disabled', 'disabled');
+    }
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+btnStopEl.addEventListener('click', () => {
+  clearInterval(timerId);
+  btnStartEl.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
